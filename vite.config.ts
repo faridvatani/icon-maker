6d3d3e94adcca80eb0884d550307fcd1a610bfb2
Farid Vatani <faridvatani@gmail.com>
2024-09-29 01:08:06 +0330

    chore: Update base URL in vite.config.ts to "/icon-maker/"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/icon-maker/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
